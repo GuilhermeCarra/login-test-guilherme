c5d9eb97e54c22330b713e5ad691493c28128e9f
Guilherme Carra <gui_gc@hotmail.com>
2020-11-30 11:59:22 +0100

    Remove: Home page route

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import { Switch, Route } from "react-router-dom";
 
 import ROUTES from "./utils/routes";
 
-import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login/Login";
 import Signup from "./pages/Auth/Signup/Signup";
 
 function App() {
   return (
     <Switch>
-      <Route path={ROUTES.HOME} exact>
-        <Home />
-      </Route>
-      <Route path={ROUTES.LOGIN}>
+      <Route path={ROUTES.LOGIN} exact>
         <Login />
       </Route>
       <Route path={ROUTES.SIGNUP}>
